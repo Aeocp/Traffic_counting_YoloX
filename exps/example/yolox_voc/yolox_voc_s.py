af68190196b2f203ca25fba7698acac3fa8c4d28
wk-earn <90698883+wk-earn@users.noreply.github.com>
2021-12-22 16:11:10 +0700

    Update yolox_voc_s.py

--- exps/example/yolox_voc/yolox_voc_s.py
+++ exps/example/yolox_voc/yolox_voc_s.py
@@ -11,7 +11,7 @@ from yolox.data import get_yolox_datadir
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
-        self.num_classes = 20
+        self.num_classes = 4
         self.depth = 0.33
         self.width = 0.50
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
