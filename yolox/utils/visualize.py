22f4d202a46cc27c7cdd42fbd8855ef660fdfb70
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2022-01-13 20:46:36 +0700

    Update visualize.py

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -14,35 +14,37 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
     #with open("/content/drive/MyDrive/hlp-01-20210610-205037-cut.mp4.txt", "a") as writefile:
       #writefile.write("# Frame %d, %d" % (mmglobal.frame_count, len(boxes))+"\n")
       #print("# Frame %d, %d" % (mmglobal.frame_count, len(boxes)))
-
+    
+    txt_size = []
     for i in range(len(boxes)):
-        box = boxes[i]
-        cls_id = int(cls_ids[i])
-        score = scores[i]
-        if score < conf:
-            continue
-        x0 = int(box[0])
-        y0 = int(box[1])
-        x1 = int(box[2])
-        y1 = int(box[3])
+        if class_names[cls_id] == "car":
+            box = boxes[i]
+            cls_id = int(cls_ids[i])
+            score = scores[i]
+            if score < conf:
+                continue
+            x0 = int(box[0])
+            y0 = int(box[1])
+            x1 = int(box[2])
+            y1 = int(box[3])
 
-        color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
-        text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
-        txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
-        font = cv2.FONT_HERSHEY_SIMPLEX
+            color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
+            text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
+            txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
-        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
-        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+            txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+            cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
 
-        txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
-        cv2.rectangle(
-            img,
-            (x0, y0 + 1),
-            (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
-            txt_bk_color,
-            -1
-        )
-        cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+            txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+            cv2.rectangle(
+                img,
+                (x0, y0 + 1),
+                (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+                txt_bk_color,
+                -1
+            )
+            cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
 
         # Hui log
 
@@ -51,9 +53,10 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         #with open("/content/drive/MyDrive/hlp-01-20210610-205037-cut.mp4.txt", "a") as writefile:
           #writefile.write("%d,%s,%d,%d,%d,%d,%.2f,%d" % (i, class_names[cls_id], x0, y0, x1, y1, score * 100, mmglobal.frame_count)+"\n")
     #file.close()
-    text = 'Frame {} count {}'.format(mmglobal.frame_count, len(boxes))
-    txt_color = (0, 0, 255)
-    cv2.putText(img, text, (2, 2 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+    if len(txt_size):
+        text = 'Frame {} count {}'.format(mmglobal.frame_count, len(boxes))
+        txt_color = (0, 0, 255)
+        cv2.putText(img, text, (2, 2 + txt_size[1]), font, 0.4, txt_color, thickness=1)
 
     return img
 
