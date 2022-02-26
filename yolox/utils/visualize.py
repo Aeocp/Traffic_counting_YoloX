5bf614634c773e6bd3230f1b6c28f9f13b43d8c0
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2022-02-26 22:06:37 +0700

    Update visualize.py

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -19,32 +19,31 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
     for i in range(len(boxes)):
         box = boxes[i]
         cls_id = int(cls_ids[i])
-        if class_names[cls_id] == "car":
-            score = scores[i]
-            if score < conf:
-                continue
-            x0 = int(box[0])
-            y0 = int(box[1])
-            x1 = int(box[2])
-            y1 = int(box[3])
+        score = scores[i]
+        if score < conf:
+            continue
+        x0 = int(box[0])
+        y0 = int(box[1])
+        x1 = int(box[2])
+        y1 = int(box[3])
 
-            color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
-            text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
-            txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
-            font = cv2.FONT_HERSHEY_SIMPLEX
+        color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
+        text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
+        txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
+        font = cv2.FONT_HERSHEY_SIMPLEX
 
-            txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
-            cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
 
-            txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
-            cv2.rectangle(
-                img,
-                (x0, y0 + 1),
-                (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
-                txt_bk_color,
-                -1
-            )
-            cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+        txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+        cv2.rectangle(
+            img,
+            (x0, y0 + 1),
+            (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+            txt_bk_color,
+            -1
+        )
+        cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
 
         # Hui log
 
